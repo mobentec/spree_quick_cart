2ccf990557960fc01a4865bdb64513867e40e20a
mobentec <mmahdi@mobentec.com>
2015-03-30 18:57:05 -0400

    Update quick_cart_add_button.rb

--- app/overrides/quick_cart_add_button.rb
+++ app/overrides/quick_cart_add_button.rb
@@ -2,5 +2,5 @@ Deface::Override.new(:virtual_path => "spree/shared/_products",
                      :name => "quick_cart_add_button",
                      :insert_after => "#products span.price",
                      :partial => "spree/shared/quick_cart_add_button",
-                     :disabled => false,
+                     :disabled => true,
                      :original => '10eb9a198f4f0938e075ba8f41c4a73a1b6a77e4')
